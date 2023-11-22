c0d4d2cb09dba9ca62d84e8c16e276a76eff21a4
JHYOOOOON <jhyooooooon@gmail.com>
2023-11-22 17:45:23 +0900

    chore: color theme 선언

---
+++ theme/colors.ts
@@ -0,0 +1,11 @@
+export const colors = {
+  skyblue: '#44CEC6',
+  red: '#FF5146',
+  black: '#333333',
+  gray: '#84858F',
+  gray100: '#999999',
+  gray200: '#C4C4C4',
+  gray300: '#EAE9ED',
+  gray400: '#F0F0F0',
+  white: '#FFFFFF',
+};
